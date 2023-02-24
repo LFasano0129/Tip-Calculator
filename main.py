095bf145fb9a32068de8b3af076e64b441829b91
Logan Fasano <58754739+LFasano0129@users.noreply.github.com>
2023-02-24 09:26:54 -0500

    final
    code completed.

--- main.py
+++ main.py
@@ -5,27 +5,19 @@ print("Welcome to the tip calculator")
 total_bill = input("What was the total bill?\n")
 total_bill_int = int(total_bill)
 
-tip = 0
-
-# loop to take input for tip percent and convert to correct float for the final computation
-while tip == 0:
-    tip_percent = input("What percentage of tip would you like to give? 10, 12, 15 or 20?\n")
-    tip_percent_int = int(tip_percent)
-    if tip_percent_int == 10:
-        tip = 1.10
-    elif tip_percent_int == 12:
-        tip = 1.12
-    elif tip_percent_int == 15:
-        tip = 1.15
-    elif tip_percent_int == 20:
-        tip = 1.20
-    else:
-        print("Invalid tip amount, please retry")
+# loop to take input for tip percent and convert to correct float for the computation
+tip_percent = input("What percentage of tip would you like to give? 10, 12, 15 or 20?\n")
+tip_percent_int = int(tip_percent)
 
 # take input for the total people who will split the bill and convert the variable to an integer
 total_ppl = input("How many people will split the bill?\n")
 total_ppl_int = int(total_ppl)
 
-# final computation to calculate what each person should pay and then display it to the user
-payment_per_person = total_bill_int / total_ppl_int * tip
+# calculate the total bill with tip
+Bill_with_tip = tip_percent_int / 100 * total_bill_int + total_bill_int
+
+# calculate the amount each person should pay
+payment_per_person = Bill_with_tip / total_ppl_int
+
+# f string to display the amount each person should pay
 print(f"Each person should pay: {round(payment_per_person, 2)}")
